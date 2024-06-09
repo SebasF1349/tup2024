e8e8763d153d26200e30e8259c328d47e99196d4
SebasF1349 <ratablanca711@gmail.com>
2024-06-09 17:18:49 -0300

    feat: darDeAltaCuenta tests

--- src/test/java/ar/edu/utn/frbb/tup/service/CuentaServiceTest.java
+++ src/test/java/ar/edu/utn/frbb/tup/service/CuentaServiceTest.java
@@ -1,7 +1,16 @@
 package ar.edu.utn.frbb.tup.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import ar.edu.utn.frbb.tup.model.Cuenta;
+import ar.edu.utn.frbb.tup.model.TipoCuenta;
+import ar.edu.utn.frbb.tup.model.TipoMoneda;
+import ar.edu.utn.frbb.tup.model.exception.CuentaAlreadyExistsException;
+import ar.edu.utn.frbb.tup.model.exception.TipoCuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.persistence.CuentaDao;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +23,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class CuentaServiceTest {
 
   @Mock private CuentaDao cuentaDao;
+  @Mock private ClienteService clienteService;
+  private final long clienteDni = 12345678;
 
   @InjectMocks private CuentaService cuentaService;
 
@@ -27,4 +38,66 @@ public class CuentaServiceTest {
   //    2 - cuenta no soportada
   //    3 - cliente ya tiene cuenta de ese tipo
   //    4 - cuenta creada exitosamente
+  @Test
+  public void testDarDeAltaCuentaExistenteException() {
+    Cuenta cuenta = createCuenta();
+
+    when(cuentaDao.find(cuenta.getNumeroCuenta())).thenReturn(cuenta);
+
+    assertThrows(
+        CuentaAlreadyExistsException.class, () -> cuentaService.darDeAltaCuenta(cuenta, 12345678));
+  }
+
+  @Test
+  public void testDarDeAltaCuentaNoSoportadaException() {
+    Cuenta cuenta = createCuenta(TipoMoneda.DOLARES, TipoCuenta.CUENTA_CORRIENTE);
+
+    when(cuentaDao.find(cuenta.getNumeroCuenta())).thenReturn(null);
+
+    assertThrows(
+        CuentaNoSoportadaException.class, () -> cuentaService.darDeAltaCuenta(cuenta, 12345678));
+  }
+
+  @Test
+  public void testDarDeAltaCuentaDuplicadaException() throws TipoCuentaAlreadyExistsException {
+    Cuenta cuenta = createCuenta();
+
+    when(cuentaDao.find(cuenta.getNumeroCuenta())).thenReturn(null);
+    doThrow(TipoCuentaAlreadyExistsException.class)
+        .when(clienteService)
+        .agregarCuenta(cuenta, clienteDni);
+
+    assertThrows(
+        TipoCuentaAlreadyExistsException.class,
+        () -> cuentaService.darDeAltaCuenta(cuenta, clienteDni));
+  }
+
+  @Test
+  public void testDarDeAltaCuentaSuccess()
+      throws TipoCuentaAlreadyExistsException,
+          CuentaNoSoportadaException,
+          CuentaAlreadyExistsException {
+    Cuenta cuenta = createCuenta();
+
+    when(cuentaDao.find(cuenta.getNumeroCuenta())).thenReturn(null);
+    doNothing().when(clienteService).agregarCuenta(cuenta, clienteDni);
+
+    cuentaService.darDeAltaCuenta(cuenta, clienteDni);
+
+    verify(cuentaDao, times(1)).find(cuenta.getNumeroCuenta());
+    assertTrue(cuentaService.tipoCuentaEstaSoportada(cuenta));
+    verify(clienteService, times(1)).agregarCuenta(cuenta, clienteDni);
+    verify(cuentaDao, times(1)).save(cuenta);
+  }
+
+  private Cuenta createCuenta() {
+    return new Cuenta()
+        .setMoneda(TipoMoneda.PESOS)
+        .setBalance(500000)
+        .setTipoCuenta(TipoCuenta.CAJA_AHORRO);
+  }
+
+  private Cuenta createCuenta(TipoMoneda moneda, TipoCuenta cuenta) {
+    return new Cuenta().setMoneda(moneda).setBalance(500000).setTipoCuenta(cuenta);
+  }
 }
