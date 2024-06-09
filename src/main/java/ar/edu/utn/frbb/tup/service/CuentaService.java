4ef113372a5bca150b87b1324c0d199f002d7d7e
SebasF1349 <ratablanca711@gmail.com>
2024-06-09 15:28:00 -0300

    feat: add tipoCuentaEstaSoportada to CuentaService

--- src/main/java/ar/edu/utn/frbb/tup/service/CuentaService.java
+++ src/main/java/ar/edu/utn/frbb/tup/service/CuentaService.java
@@ -1,40 +1,71 @@
 package ar.edu.utn.frbb.tup.service;
 
 import ar.edu.utn.frbb.tup.model.Cuenta;
+import ar.edu.utn.frbb.tup.model.TipoCuenta;
+import ar.edu.utn.frbb.tup.model.TipoMoneda;
 import ar.edu.utn.frbb.tup.model.exception.CuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.model.exception.TipoCuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.persistence.CuentaDao;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Component
 public class CuentaService {
-    CuentaDao cuentaDao = new CuentaDao();
+  CuentaDao cuentaDao = new CuentaDao();
 
-    @Autowired
-    ClienteService clienteService;
+  private final Map<TipoCuenta, List<TipoMoneda>> cuentasSoportadas =
+      Map.ofEntries(
+          new AbstractMap.SimpleEntry<TipoCuenta, List<TipoMoneda>>(
+              TipoCuenta.CAJA_AHORRO, Arrays.asList(TipoMoneda.PESOS, TipoMoneda.DOLARES)),
+          new AbstractMap.SimpleEntry<TipoCuenta, List<TipoMoneda>>(
+              TipoCuenta.CUENTA_CORRIENTE, Arrays.asList(TipoMoneda.PESOS)));
 
-    //Generar casos de test para darDeAltaCuenta
-    //    1 - cuenta existente
-    //    2 - cuenta no soportada
-    //    3 - cliente ya tiene cuenta de ese tipo
-    //    4 - cuenta creada exitosamente
-    public void darDeAltaCuenta(Cuenta cuenta, long dniTitular) throws CuentaAlreadyExistsException, TipoCuentaAlreadyExistsException {
-        if(cuentaDao.find(cuenta.getNumeroCuenta()) != null) {
-            throw new CuentaAlreadyExistsException("La cuenta " + cuenta.getNumeroCuenta() + " ya existe.");
-        }
+  @Autowired ClienteService clienteService;
 
-        //Chequear cuentas soportadas por el banco CA$ CC$ CAU$S
-        // if (!tipoCuentaEstaSoportada(cuenta)) {...}
-
-        clienteService.agregarCuenta(cuenta, dniTitular);
-        cuentaDao.save(cuenta);
+  // Generar casos de test para darDeAltaCuenta
+  //    1 - cuenta existente
+  //    2 - cuenta no soportada
+  //    3 - cliente ya tiene cuenta de ese tipo
+  //    4 - cuenta creada exitosamente
+  public void darDeAltaCuenta(Cuenta cuenta, long dniTitular)
+      throws CuentaAlreadyExistsException,
+          TipoCuentaAlreadyExistsException,
+          CuentaNoSoportadaException {
+    if (cuentaDao.find(cuenta.getNumeroCuenta()) != null) {
+      throw new CuentaAlreadyExistsException(
+          "La cuenta " + cuenta.getNumeroCuenta() + " ya existe.");
     }
 
-    public Cuenta find(long id) {
-        return cuentaDao.find(id);
+    // Chequear cuentas soportadas por el banco CA$ CC$ CAU$S
+    if (!tipoCuentaEstaSoportada(cuenta)) {
+      throw new CuentaNoSoportadaException(
+          "El tipo de cuenta "
+              + cuenta.getTipoCuenta()
+              + " no soporta la moneda "
+              + cuenta.getMoneda());
     }
+
+    clienteService.agregarCuenta(cuenta, dniTitular);
+    cuentaDao.save(cuenta);
+  }
+
+  public Cuenta find(long id) {
+    return cuentaDao.find(id);
+  }
+
+  public boolean tipoCuentaEstaSoportada(Cuenta cuenta) {
+    TipoMoneda moneda = cuenta.getMoneda();
+    TipoCuenta tipoCuenta = cuenta.getTipoCuenta();
+    if (!cuentasSoportadas.containsKey(tipoCuenta)) {
+      return false;
+    }
+    if (!cuentasSoportadas.get(tipoCuenta).contains(moneda)) {
+      return false;
+    }
+    return true;
+  }
 }
